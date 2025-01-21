374ea3c3c4e73acf44ac6472c91d0ab0d9d73aa1
Sanjana42475 <sanjanadewangan23@navgurukul.org>
2025-01-21 21:46:58 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,9 @@
+StudentManage.prototype.addStudent = function (name,age,grades
+) {
+    this.students.push({
+        name: name,
+        age: age,
+        grades: grades
+    })
+}
+console.log()
